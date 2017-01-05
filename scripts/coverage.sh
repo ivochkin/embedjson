19addc56a7e1b2e22abed3ec98c50d85f2a36c1c
Stanislav Ivochkin <isn@extrn.org>
2017-01-05 17:04:20 +0300

    Fix gcov on travis-ci

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -22,6 +22,6 @@ else
   pushd doc/coverage
   genhtml lcov.info
   echo
-  echo Open $(pwd)/index.html in your browser for the coverage report
+  echo Open file://$(pwd)/index.html in your browser for the coverage report
   popd >/dev/null
 fi
