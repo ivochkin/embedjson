973cc4cca33030534920b4fe489915d08e85724b
Stanislav Ivochkin <isn@extrn.org>
2017-01-05 12:59:02 +0300

    devenv: Add travis.ci and codecov.io

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # embedjson
 [![License MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/ivochkin/embedjson/master/LICENSE)
+[![Build Status](https://travis-ci.org/ivochkin/embedjson.svg?branch=master)](https://travis-ci.org/ivochkin/embedjson)
+[![codecov.io](https://codecov.io/github/ivochkin/embedjson/coverage.svg?branch=master)](https://codecov.io/github/ivochkin/embedjson?branch=master)
 
 SAX-style JSON parser, a component of jcppy code generator (https://github.com/ivochkin/jcppy)
---
+++ scripts/coverage.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Create test coverage report
+#
+# Prerequisite:
+# * installed lcov
+# * code is built with --coverage -O0
+#
+# Can be run in two modes:
+# * default (no special environment variables set) - create local html report
+# * CI ("$TRAVIS" == "true") - upload coverage report to codecov.io
+
+lcov --directory . --capture --output-file lcov.info
+lcov --remove lcov.info '*ut_*' --output lcov.info
+
+if [ "$TRAVIS" == "true" ]; then
+  # deploy
+  bash <(curl -s https://codecov.io/bash)
+else
+  # generate html localy
+  mkdir -p doc/coverage
+  mv lcov.info doc/coverage/
+  pushd doc/coverage
+  genhtml lcov.info
+  echo
+  echo Open $(pwd)/index.html in your browser for the coverage report
+  popd >/dev/null
+fi
